725d43ebe8d26dd27bbdd004688e7f3c7e6d9df0
Jesmin Baidya <77732694+jemin09@users.noreply.github.com>
2021-07-13 16:00:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>donation page</title>
+     <link rel="stylesheet" href="style1.css">
+
+    <?php include "link.php";?>
+  
+</head>
+<body class="bg" >
+        
+      
+     <nav class="navbar navbar-expand-md  navbar-dark sticky-top "  style="background-color: #5BE7CA ; ; opacity: .9; " >
+          <img src="charity.jpg" alt="Logo" style="width:42px; border-radius:40%">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" >
+               <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+               <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php">Home</a>
+               </li>
+               <li class="nav-item ">
+                    <a class="nav-link text-white" href="#">Contact</a>
+               </li>
+               <li class="nav-item">
+                    <a class="nav-link text-white" href="https://www.charitynavigator.org/">Find out more</a>
+               </li>    
+          </ul>
+          </div>  
+     </nav>
+
+
+     <div class="container-fluid ">
+          <div class="row justify-content-center bg-dark">
+           
+          <div class="contain">          
+               <h5><b> “We make a living by what we get, but we make a life by what we give.”</b></h5>
+               <i>―Winston Churchill</i>
+          </div>
+
+          </div>
+          </div>  
+     
+     
+     <div class="donation" >
+          <form  id="btn"><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HXiM1dHXdcVHnR" async> </script> </form>
+               
+     </div>             
+                                         
+     <div class="footer">
+          <p>Copyright &copy; 2021|Jesmin|All rights reserved</p>
+     </div>
+    
+    
+</body>
+</html>
+
+
+
+    
